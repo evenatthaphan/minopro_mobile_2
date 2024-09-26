ed754b890a6d4ddda5bfb77025070ca5f2b8a8f4
nsisthan@gmail.com <162254547+evenatthaphan@users.noreply.github.com>
2024-09-27 01:15:30 +0700

    eve update

--- lib/page/user/register_user.dart
+++ lib/page/user/register_user.dart
@@ -8,6 +8,12 @@ class RegirterPage extends StatefulWidget {
 }
 
 class _RegirterPageState extends State<RegirterPage> {
+  TextEditingController name = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController phone = TextEditingController();
+  TextEditingController wallet = TextEditingController();
+  TextEditingController confirmPassword = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
@@ -16,8 +22,8 @@ class _RegirterPageState extends State<RegirterPage> {
         //   automaticallyImplyLeading: false,  // ซ่อนลูกศรชี้กลับ
         //   ),
         body: SingleChildScrollView(
-          child: Stack(
-                children: [
+      child: Stack(
+        children: [
           SizedBox(
             width: screenSize.width * 1, // ขยายความกว้างของ Card
             height: screenSize.height * 0.25, // ขยายความสูงของ Card
@@ -25,23 +31,40 @@ class _RegirterPageState extends State<RegirterPage> {
               color: Color.fromARGB(255, 247, 182, 254),
               child: Container(
                 child: Padding(
-                  padding: EdgeInsets.only(top: screenSize.height * 0.05,
-                  left: screenSize.width * 0.05),
+                  padding: EdgeInsets.only(
+                      top: screenSize.height * 0.05,
+                      left: screenSize.width * 0.05),
                   child: Row(
                     children: [
                       SizedBox(
-                          child: Image.asset(
-                            'assets/images/user.png',
-                            width: screenSize.width * 0.3,
-                          ),
+                        child: Image.asset(
+                          'assets/images/user.png',
+                          width: screenSize.width * 0.3,
                         ),
-                      Text(
-                          "ลงทะเบียนผู้ใช้",
-                          style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.purple,
-                              fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: screenSize.width * 0.05),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "ผู้ใช้ทั่วไป",
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.purple[400],
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const Text(
+                              "ลงทะเบียนผู้ใช้",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
                         ),
+                      ),
                     ],
                   ),
                 ),
@@ -49,12 +72,149 @@ class _RegirterPageState extends State<RegirterPage> {
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(top: screenSize.height * 0.26),
-            child: Center(),
-          )
+              padding: EdgeInsets.only(top: screenSize.height * 0.26,
+              left: screenSize.width*0.05, right: screenSize.width*0.05),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('ชื่อ-สกุล : '),
+                  TextField(
+                    controller: name,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                        ),
+                      ),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 247, 182, 254),
+                            width: 2), // เปลี่ยนขอบเป็นสีแดงเมื่อคลิก
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: screenSize.height * 0.02),
+                    child: const Text('อีเมลล์ : '),
+                  ),
+                  TextField(
+                    controller: email,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                        ),
+                      ),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 247, 182, 254),
+                            width: 2), // เปลี่ยนขอบเป็นสีแดงเมื่อคลิก
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: screenSize.height * 0.02),
+                    child: const Text('เบอร์โทรศัพท์ : '),
+                  ),
+                  TextField(
+                    maxLength: 10,
+                    controller: phone,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                        ),
+                      ),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 247, 182, 254),
+                            width: 2), // เปลี่ยนขอบเป็นสีแดงเมื่อคลิก
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: screenSize.height * 0.02),
+                    child: const Text('รหัสผ่าน : '),
+                  ),
+                  TextField(
+                    controller: password,
+                    // obscureText:
+                    //     !_isPasswordVisible, // This line only needs to appear once
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                        ),
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 10),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 247, 182, 254),
+                            width: 2), // เปลี่ยนขอบเป็นสีแดงเมื่อคลิก
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          // _isPasswordVisible
+                          //     ? Icons.visibility
+                          Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          // setState(() {
+                          //   _isPasswordVisible = !_isPasswordVisible;
+                          // });
+                        },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: screenSize.height * 0.02),
+                    child: const Text('ยืนยันรหัสผ่าน : '),
+                  ),
+                  SizedBox(height: screenSize.height * 0.015),
+                  TextField(
+                    // controller: confirmPassword,
+                    // obscureText:
+                    //     !_isPasswordVisible1, // This line only needs to appear once
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                        ),
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 10),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 247, 182, 254),
+                            width: 2), // เปลี่ยนขอบเป็นสีแดงเมื่อคลิก
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          // _isPasswordVisible1
+                          //     ? Icons.visibility
+                          Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            // _isPasswordVisible1 = !_isPasswordVisible1;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
                 ],
-              ),
-        ));
+              ))
+        ],
+      ),
+    ));
   }
 
   void registeruser() {}
--- lib/page/user/sender/HmoePageSender.dart
+++ lib/page/user/sender/HmoePageSender.dart
@@ -13,48 +13,49 @@ class _HomePageSenderState extends State<HomePageSender> {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            SizedBox(
-              width: screenSize.width, // ขยายความกว้างของ Card
-              height: screenSize.height * 0.25, // ขยายความสูงของ Card
-              child: Card(
-                color: Color.fromARGB(255, 185, 121, 250),
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    top: screenSize.height * 0.07,
-                  ),
-                  child: Container(
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                        left: screenSize.width * 0.10,
-                        right: screenSize.width * 0.10,
-                        top: screenSize.height * 0.02,
-                      ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true, // Enables the fill color
-                          fillColor:
-                              Colors.white, // Sets the fill color to white
-                          hintText: 'ค้นหาผู้รับ', // Placeholder text
-                          border: const OutlineInputBorder(
-                            borderSide: BorderSide(width: 1),
-                          ),
-                          focusedBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color.fromRGBO(108, 3, 104, 1),
-                                width: 2),
-                          ),
-                          suffixIcon: IconButton(
-                            icon: const Icon(
-                              Icons.search,
+        body: SingleChildScrollView(
+          child: Stack(
+            children: [
+              SizedBox(
+                width: screenSize.width, // ขยายความกว้างของ Card
+                height: screenSize.height * 0.25, // ขยายความสูงของ Card
+                child: Card(
+                  color: Color.fromARGB(255, 185, 121, 250),
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      top: screenSize.height * 0.07,
+                    ),
+                    child: Container(
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: screenSize.width * 0.10,
+                          right: screenSize.width * 0.10,
+                          top: screenSize.height * 0.02,
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true, // Enables the fill color
+                            fillColor:
+                                Colors.white, // Sets the fill color to white
+                            hintText: 'ค้นหาผู้รับ', // Placeholder text
+                            border: const OutlineInputBorder(
+                              borderSide: BorderSide(width: 1),
                             ),
-                            onPressed: () {
-                              setState(() {
-                                // Handle search logic
-                              });
-                            },
+                            focusedBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromRGBO(108, 3, 104, 1),
+                                  width: 2),
+                            ),
+                            suffixIcon: IconButton(
+                              icon: const Icon(
+                                Icons.search,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  // Handle search logic
+                                });
+                              },
+                            ),
                           ),
                         ),
                       ),
@@ -62,94 +63,106 @@ class _HomePageSenderState extends State<HomePageSender> {
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: screenSize.height * 0.25, left: screenSize.width * 0.05),
-              child: Column(
-                children: [
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'ผลการค้นหา :',
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.purple),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: screenSize.width, // ขยายความกว้างของ Card
-                        height: screenSize.height * 0.13,
-                        child: Padding(
-                          padding: EdgeInsets.only(
-                            right: screenSize.width * 0.05,
-                            top: screenSize.height * 0.02,
-                          ),
-                          child: Card.outlined(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Image.asset(
-                                  'assets/images/profile.png',
-                                  width: screenSize.width * 0.25,
-                                ),
-                                const Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Column(
+              Padding(
+                padding: EdgeInsets.only(
+                    top: screenSize.height * 0.25,
+                    left: screenSize.width * 0.05),
+                child: Column(
+                  children: [
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'ผลการค้นหา :',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: screenSize.width, // ขยายความกว้างของ Card
+                          height: screenSize.height * 0.13,
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                              right: screenSize.width * 0.05,
+                              top: screenSize.height * 0.02,
+                            ),
+                            child: Card.outlined(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
                                     children: [
-                                      Text(
-                                        'น้องแม็ก',
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.black),
+                                      Image.asset(
+                                        'assets/images/profile.png',
+                                        width: screenSize.width * 0.25,
                                       ),
-                                      Text(
-                                        '0254718963',
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.purple),
+                                      const Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              'น้องแม็ก',
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.black),
+                                            ),
+                                            Text(
+                                              '0254718963',
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.purple),
+                                            ),
+                                          ],
+                                        ),
                                       ),
                                     ],
                                   ),
-                                ),
-                                FilledButton(
-                            onPressed: () {
-                              
-                            },
-                            style: ButtonStyle(
-                              backgroundColor: WidgetStateProperty.all<Color>(
-                                  const Color.fromARGB(255, 202, 127, 218)),
-                            ),
-                            child: Text(
-                              'เลือก',
-                              style:
-                                  TextStyle(fontSize: screenSize.width * 0.04),
-                            ),
-                          ),
-                              ],
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: FilledButton(
+                                      onPressed: () {},
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                            WidgetStateProperty.all<Color>(
+                                                const Color.fromARGB(
+                                                    255, 202, 127, 218)),
+                                      ),
+                                      child: Text(
+                                        'เลือก',
+                                        style: TextStyle(
+                                            fontSize: screenSize.width * 0.04),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar: UserNavbarPage(
-        selectedIndex: 0,
-        onDestinationSelected: (value) {},
-        screenSize: screenSize,)
-    );
+        bottomNavigationBar: UserNavbarPage(
+          selectedIndex: 0,
+          onDestinationSelected: (value) {},
+          screenSize: screenSize,
+        ));
   }
 }
